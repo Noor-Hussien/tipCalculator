3fe207a45ebbdca4f7bdb6a7376883c802db7acc
Noor Ali <profesanet@gmail.com>
2020-08-02 19:51:55 -0400

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Noor Ali on 8/2/20.
+//  Copyright © 2020 Noor Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
