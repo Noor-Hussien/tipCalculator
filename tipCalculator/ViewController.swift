e127d46dcf2512f7ea2647bf4ff1419b99b1de81
Noor Ali <profesanet@gmail.com>
2020-08-02 21:57:53 -0400

    Calculating tip added Calculating total added Tap Gesture added

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        
+//        print("Hello")
+        view.endEditing(true) // makes the keyboard to disappear by activating tap Gesture
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get the biil amount
+        let bill = Double(billTextField.text!) ?? 0 // set to zero if n't provided
+        let tipPercentage = [0.15, 0.18, 0.20]
+        
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]    // Calculate the bill
+        let total = tip + bill     // calculate total
+        
+        tipLabel.text    =  String(format: "$%.2f", tip)
+        totalLabel.text  =  String(format: "$%.2f", total)
+    }
 }
 
